2ccde9ac736bf2f6d1a90d01f7af1b0e5bbe9b49
Paul Yun <paul.a.yun@gmail.com>
2022-09-01 20:46:40 -0700

    Add content

--- cms/config.js
+++ cms/config.js
@@ -12,28 +12,22 @@ export default {
     {
       name: 'pages',
       label: 'Pages',
-      files: [
+      folder: 'content/pages',
+      fields: [
         {
-          label: 'Home',
-          name: 'home',
-          file: 'content/pages/home.md',
-          fields: [
-            {
-              label: 'Hero Title',
-              name: 'hero_title',
-              widget: 'string',
-            },
-            {
-              label: 'Hero Description',
-              name: 'hero_description',
-              widget: 'markdown',
-            },
-            {
-              label: 'Hero Image',
-              name: 'hero_image',
-              widget: 'image',
-            },
-          ],
+          label: 'Hero Title',
+          name: 'hero_title',
+          widget: 'string',
+        },
+        {
+          label: 'Hero Description',
+          name: 'hero_description',
+          widget: 'markdown',
+        },
+        {
+          label: 'Hero Image',
+          name: 'hero_image',
+          widget: 'image',
         },
       ],
     },
